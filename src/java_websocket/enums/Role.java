f447250e638aec378175e6c54b604fb639e9316f
deon4e <deon4e@gmail.com>
2021-06-03 10:45:24 +0200

    Cleaning

--- src/java_websocket/enums/Role.java
+++
@@ -1,8 +0,0 @@
-package java_websocket.enums;
-
-/**
- * Enum which represents the states a websocket may be in
- */
-public enum Role {
-  CLIENT, SERVER
-}
--- src/slf4j/spi/LoggingEventAware.java
+++
@@ -1,9 +0,0 @@
-package slf4j.spi;
-
-import slf4j.event.LoggingEvent;
-
-public interface LoggingEventAware {
-
-	void log(LoggingEvent event);
-	
-}

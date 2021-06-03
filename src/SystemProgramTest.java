f3505beb1304eeb7ccf281f49ebc6f7015a96cf2
deon4e <deon4e@gmail.com>
2021-06-03 23:16:18 +0200

    Merge

---
+++ src/SystemProgramTest.java
@@ -0,0 +1,37 @@
+
+import org.p2p.solanaj.core.PublicKey;
+import org.p2p.solanaj.core.TransactionInstruction;
+import org.p2p.solanaj.programs.SystemProgram;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.bitcoinj.core.Base58;
+
+public class SystemProgramTest {
+
+    @Test
+    public void transferInstruction() {
+        PublicKey fromPublicKey = new PublicKey("QqCCvshxtqMAL2CVALqiJB7uEeE5mjSPsseQdDzsRUo");
+        PublicKey toPublickKey = new PublicKey("GrDMoeqMLFjeXQ24H56S1RLgT4R76jsuWCd6SvXyGPQ5");
+        int lamports = 3000;
+
+        TransactionInstruction instruction = SystemProgram.transfer(fromPublicKey, toPublickKey, lamports);
+
+        assertEquals(SystemProgram.PROGRAM_ID, instruction.getProgramId());
+        assertEquals(2, instruction.getKeys().size());
+        assertEquals(toPublickKey, instruction.getKeys().get(1).getPublicKey());
+
+        assertArrayEquals(new byte[] { 2, 0, 0, 0, -72, 11, 0, 0, 0, 0, 0, 0 }, instruction.getData());
+    }
+
+    @Test
+    public void createAccountInstruction() {
+        TransactionInstruction instruction = SystemProgram.createAccount(SystemProgram.PROGRAM_ID,
+                SystemProgram.PROGRAM_ID, 2039280, 165, SystemProgram.PROGRAM_ID);
+
+        assertEquals("11119os1e9qSs2u7TsThXqkBSRUo9x7kpbdqtNNbTeaxHGPdWbvoHsks9hpp6mb2ed1NeB",
+                Base58.encode(instruction.getData()));
+    }
+
+}
---
+++ src/org/p2p/solanaj/programs/SystemProgram.java
@@ -0,0 +1,44 @@
+package org.p2p.solanaj.programs;
+
+import java.util.ArrayList;
+
+import org.p2p.solanaj.core.PublicKey;
+import org.p2p.solanaj.core.TransactionInstruction;
+import org.p2p.solanaj.core.AccountMeta;
+
+import static org.bitcoinj.core.Utils.*;
+
+public class SystemProgram {
+    public static final PublicKey PROGRAM_ID = new PublicKey("11111111111111111111111111111111");
+
+    public static final int PROGRAM_INDEX_CREATE_ACCOUNT = 0;
+    public static final int PROGRAM_INDEX_TRANSFER = 2;
+
+    public static TransactionInstruction transfer(PublicKey fromPublicKey, PublicKey toPublickKey, long lamports) {
+        ArrayList<AccountMeta> keys = new ArrayList<AccountMeta>();
+        keys.add(new AccountMeta(fromPublicKey, true, true));
+        keys.add(new AccountMeta(toPublickKey, false, true));
+
+        // 4 byte instruction index + 8 bytes lamports
+        byte[] data = new byte[4 + 8];
+        uint32ToByteArrayLE(PROGRAM_INDEX_TRANSFER, data, 0);
+        int64ToByteArrayLE(lamports, data, 4);
+
+        return new TransactionInstruction(PROGRAM_ID, keys, data);
+    }
+
+    public static TransactionInstruction createAccount(PublicKey fromPublicKey, PublicKey newAccountPublikkey,
+            long lamports, long space, PublicKey programId) {
+        ArrayList<AccountMeta> keys = new ArrayList<AccountMeta>();
+        keys.add(new AccountMeta(fromPublicKey, true, true));
+        keys.add(new AccountMeta(newAccountPublikkey, true, true));
+
+        byte[] data = new byte[4 + 8 + 8 + 32];
+        uint32ToByteArrayLE(PROGRAM_INDEX_CREATE_ACCOUNT, data, 0);
+        int64ToByteArrayLE(lamports, data, 4);
+        int64ToByteArrayLE(space, data, 12);
+        System.arraycopy(programId.toByteArray(), 0, data, 20, 32);
+
+        return new TransactionInstruction(PROGRAM_ID, keys, data);
+    }
+}
